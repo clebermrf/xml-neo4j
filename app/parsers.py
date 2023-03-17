888838a265c1498ac1ae3ebf249840a17150796c
Cleber Marschner <clebermrf@gmail.com>
2023-03-17 11:55:16 -0300

    :clipboard: Fix Uniprot parser init

--- app/parsers.py
+++ app/parsers.py
@@ -6,43 +6,33 @@ class Uniprot:
     URL = '{http://uniprot.org/uniprot}'
 
     def __init__(self, file):
-
-        tree = ET.fromstring(file)
-        self.root = tree.getroot()  
+        self.tree = ET.fromstring(file)
 
     @property
     def accession(self) -> list:
-
-        for item in self.root.iter(self.URL + 'uniprot'):
+        for item in self.tree.iter(self.URL + 'uniprot'):
             entries = item.findall(self.URL + 'entry')
-
             for entry in entries:
                 return entry.findtext(self.URL + 'accession')
 
     @property
     def gene_primary_name(self) -> list:
-
-        for item in self.root.iter(self.URL + 'uniprot'):
+        for item in self.tree.iter(self.URL + 'uniprot'):
             entries = item.findall(self.URL + 'entry')
-
             for entry in entries:
                 return entry.find(self.URL + 'gene').find(self.URL + 'name[@type="primary"]').text
 
     @property
     def gene_synonym(self) -> list:
-
-        for item in self.root.iter(self.URL + 'uniprot'):
+        for item in self.tree.iter(self.URL + 'uniprot'):
             entries = item.findall(self.URL + 'entry')
-
             for entry in entries:
                 return entry.find(self.URL + 'gene').find(self.URL + 'name[@type="synonym"]').text
 
     @property
     def full_name(self) -> str:
-
-        for item in self.root.iter(self.URL + 'uniprot'):
+        for item in self.tree.iter(self.URL + 'uniprot'):
             entries = item.findall(self.URL + 'entry')
-
             for entry in entries:
                 return entry.find(self.URL + 'protein').find(self.URL + 'recommendedName').findtext(self.URL + 'fullName')
 
