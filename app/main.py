a1ad4121019a52ffac9b28bd4aa1e67ec64e0a3f
Cleber Marschner <clebermrf@gmail.com>
2023-03-17 12:04:34 -0300

    :ship: Implement API

---
+++ app/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, Response, APIRouter, Request
+import os
+from parsers import Uniprot
+from drivers import Neo4jConnector
+
+
+NEO4J_URI = "neo4j://localhost:7687"
+NEO4J_USER = "neo4j"
+NEO4J_PASSWORD = "adminneo4j"
+
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "Uniprot"}
+
+
+@app.post("/protein")
+async def protein(request: Request):
+
+    body = await request.body()
+    protein = Uniprot(body)
+
+    app = Neo4jConnector(NEO4J_URI, NEO4J_USER, NEO4J_PASSWORD)
+    app.create_protein(
+        protein.accession,
+        protein.full_name, 
+        protein.gene_primary_name, 
+        protein.gene_synonym
+    )
+    app.close()
